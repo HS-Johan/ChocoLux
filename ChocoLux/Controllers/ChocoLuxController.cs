3c93cab0ff2fb5747c8781c6d6f177f78c82baa8
Hasan Shahriar <johan.cse5.bu@gmail.com>
2024-12-31 03:31:00 +0600

    AllPages_31Dec24_v1

---
+++ ChocoLux/Controllers/ChocoLuxController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChocoLux.Controllers
+{
+    public class ChocoLuxController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Chocolates()
+        {
+            return View();
+        }
+        public IActionResult Testimonial()
+        {
+            return View();
+        }
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
---
+++ ChocoLux/Controllers/ContactController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChocoLux.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+    }
+}
